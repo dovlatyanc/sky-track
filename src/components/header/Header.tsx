e672fd1ee4adfb34ae5c681615d1e4848be4dc6d
dovlatyanc <dovlatyanc.ru@gmail.com>
2026-06-25 13:41:23 +0300

    addded redirect to Home when you click on logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -23,11 +23,13 @@ export function Header() {
   return (
     <div className='xs:hidden bg-card sm:px-mini-element xs:flex-col xs:pb-4 fixed top-0 lg:top-7 left-1/2 z-50 flex w-full lg:w-4/12 -translate-x-1/2 items-center justify-between rounded-b-xl lg:rounded-xl p-2 px-5 sm:rounded-lg xl:relative xl:top-0 xl:mb-5 xl:w-full shadow-md'>
       <div className='xs:flex-wrap xs:justify-center xs:mb-3 flex items-center gap-4 sm:gap-2 2xl:gap-3'>
-        <img
-          src='/logo.svg'
-          alt='Sky Track Logo'
-          className='mt-1 h-12 w-12 sm:h-10 sm:w-10'
-        />
+        <Link to={PAGES.HOME} className="flex-shrink-0">
+          <img
+            src='/logo.svg'
+            alt='Sky Track Logo'
+            className='mt-1 h-12 w-12 sm:h-10 sm:w-10 cursor-pointer'
+          />
+        </Link>
         <nav>
           <ul className='flex items-center gap-5 sm:gap-3'>
             {headerMenuData.map(item => (
